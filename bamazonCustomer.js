483ace7f9df303c3e02277b19a9a83917deea113
Tien Nguyen <tien.ng1020@gmail.com>
2019-07-18 20:11:38 -0500

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,64 +9,57 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(error){
-    if(error) throw error;
+connection.connect(function (error) {
+    if (error) throw error;
 
     console.log("id", connection.threadId);
     tabledisplay();
-    
+
 });
 
-function tabledisplay(){
+function tabledisplay() {
     var query = "SELECT item_id, product_name, department_name, price,stock_quantity FROM products ";
-    connection.query(query, function(err, res){
-        if(err) throw err;
-        
-            console.table(res);
-         
-          runSearch();
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+
+        runSearch();
     });
 }
 
-function runSearch(){
+function runSearch() {
     inquirer
-    .prompt([{
-      name: "itemwant",
-      type: "input",
-      message: "What would you like to buy? Type the product ID here: ",
-     
-    },{
-        name: "quantity",
-        type: "input",
-        message: "Enter the quantity:"
-    }])
-    .then(function(answer) {
-        
-        var query = "SELECT products.item_id, products.product_name, department_name, price, stock_quantity FROM products WHERE ?";
-        connection.query(query, { item_id: answer.itemwant },
-             function(err, res) {
-          if (err) throw err;
-          for(var i=0; i< res.length; i++){
-          if(res[i].stock_quantity < answer.quantity){
-              console.log("Insufficient quantity!")
-              runSearch();
-          }
-          else{
-          console.log("ID:",res[i].item_id,"\n",
-           "Product:", res[i].product_name,"\n",
-            "Price:", res[i].price,"\n",
-             "Quantity:", answer.quantity)
-          console.log ("Total: $", res[i].price*answer.quantity)
+        .prompt([{
+            name: "itemwant",
+            type: "input",
+            message: "What would you like to buy? Type the product ID here: ",
 
+        }, {
+            name: "quantity",
+            type: "input",
+            message: "Enter the quantity:"
+        }])
+        .then(function (answer) {
 
+            var query = "SELECT products.item_id, products.product_name, department_name, price, stock_quantity FROM products WHERE ?";
+            connection.query(query, { item_id: answer.itemwant },
+                function (err, res) {
+                    if (err) throw err;
+                    for (var i = 0; i < res.length; i++) {
+                        if (res[i].stock_quantity < answer.quantity) {
+                            console.log("Insufficient quantity!")
+                            runSearch();
+                        }
+                        else {
+                            console.log("ID:", res[i].item_id, "\n",
+                                "Product:", res[i].product_name, "\n",
+                                "Price:", res[i].price,"per item", "\n",
+                                "Quantity:", answer.quantity)
+                            console.log("Total: $", res[i].price * answer.quantity)
+                        }
 
-            }
-              
-        }
-          }
-        )
-
-
-
-})
+                    }
+                })
+            })
 }
